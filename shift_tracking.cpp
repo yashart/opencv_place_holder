5bc19c6cedd37804652648396b41a43671a62eb5
Artem Yashukhin <yashart@mail.ru>
2018-01-08 00:34:54 +0300

    add log

--- shift_tracking.cpp
+++ shift_tracking.cpp
@@ -8,7 +8,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <wiringSerial.h>
-#include <wiringPi.h>
+//#include <wiringPi.h>
+#include <time.h>
 
 #include "uart.h"
 #include "send_img_ssh.h"
@@ -17,43 +18,50 @@
 using namespace cv;
 
 void display_img(Mat frame, Point2d shift);
-void save_img(Mat frame, Point2d shift);
+void save_img(Mat frame, Point2d shift, int imgCounter, double time, int log);
 Point2d calc_shift(Mat* curr, Mat* prev, Mat* frame,
                    Mat* prev64f, Mat* curr64f, Mat* hann);
 
 
 //argv[1] - serial port
 //argv[2] - param scp
+//argv[3] - log
 int main(int argc, char* argv[])
 {
     VideoCapture video(0);
     Mat frame, curr, prev, curr64f, prev64f, hann;
-    int uartfd = serialOpen(argv[1], 115200);//open (argv[1], O_RDWR);
+    int uartfd = 1;//serialOpen(argv[1], 115200);//open (argv[1], O_RDWR);
     if (uartfd < 0)
     {
             fprintf (stderr, "error %d opening %s: %s", errno, argv[1], strerror (errno));
             return 0;
     }
-    init_uart_fd(uartfd);
+    //init_uart_fd(uartfd);
     int isFirstImg = 1;
     int resultShiftData[4] = {};
+    int imgCounter = 0;
+    time_t startTime;
+    time_t currTime;
+    time(&startTime);
     do {
         video >> frame;
         Point2d shift = calc_shift(&curr, &prev, &frame,
                                    &prev64f, &curr64f, &hann);
 
-        save_img(frame, shift);
+        time(&currTime);
+        save_img(frame, shift, imgCounter, difftime(currTime, startTime), atoi(argv[3]));
         if (isFirstImg) {
             fork_and_exec_scp("img.jpg", argv[2]);
             isFirstImg = 0;
         }
         //display_img(frame, shift);
-        //printf("x shift: %d, y shift %d\n", (int)shift.x, (int)shift.y);
+        printf("x shift: %d, y shift %d\n", (int)shift.x, (int)shift.y);
         resultShiftData[0] = (int)shift.x;
         resultShiftData[1] = (int)shift.y;
         //serialPuts(uartfd, resultShiftData);
-        write (uartfd, resultShiftData, sizeof(*resultShiftData)*3);
+        //write (uartfd, resultShiftData, sizeof(*resultShiftData)*3);
         //write(uartfd, "hi!", 4);
+        imgCounter ++;
     } while(1);
 
     return 0;
@@ -71,7 +79,7 @@ void display_img(Mat frame, Point2d shift) {
         imshow("phase shift", frame);
 }
 
-void save_img(Mat frame, Point2d shift) {
+void save_img(Mat frame, Point2d shift, int imgCounter, double time, int log) {
     double radius = cv::sqrt(shift.x*shift.x + shift.y*shift.y);
     if(radius > 5)
     {
@@ -81,6 +89,12 @@ void save_img(Mat frame, Point2d shift) {
     }
 
     imwrite("img.jpg", frame);
+    if (log) {
+        char newname[50] = {};
+        sprintf(newname, "log/%.f_%d.jpg", time, imgCounter);
+        printf("%s\n", newname);
+        imwrite(newname, frame);
+    }
 }
 
 Point2d calc_shift(Mat* curr, Mat* prev, Mat* frame,
